44e5c1c83956f21dac8f655b125832783484cf13
Jensen <neltharion1127@gmail.com>
2026-05-05 10:57:53 +0100

    add usdt

--- src/hotspot/share/utilities/dtrace_disabled.hpp
+++ src/hotspot/share/utilities/dtrace_disabled.hpp
@@ -119,6 +119,10 @@
 #define HOTSPOT_VMOPS_BEGIN_ENABLED() 0
 #define HOTSPOT_VMOPS_END(arg0, arg1, arg2)
 #define HOTSPOT_VMOPS_END_ENABLED() 0
+#define HOTSPOT_VTHREAD_FREEZE(arg0, arg1)
+#define HOTSPOT_VTHREAD_FREEZE_ENABLED() 0
+#define HOTSPOT_VTHREAD_THAW(arg0, arg1)
+#define HOTSPOT_VTHREAD_THAW_ENABLED() 0
 
 /* hs_private provider probes */
 #define HS_PRIVATE_HASHTABLE_NEW_ENTRY(arg0, arg1, arg2, arg3)
