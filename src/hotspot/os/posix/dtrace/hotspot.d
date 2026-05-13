8c863478f688326cf199836bfd37e0773731e65a
Jensen <neltharion1127@gmail.com>
2026-05-13 23:46:21 +0100

    feat: pass per-vthread off-heap trace buffer address through USDT probes

--- src/hotspot/os/posix/dtrace/hotspot.d
+++ src/hotspot/os/posix/dtrace/hotspot.d
@@ -77,8 +77,8 @@ provider hotspot {
     int, char*, int, char*, int, char*, int);
   probe method__return(
     int, char*, int, char*, int, char*, int);
-  probe vthread__freeze(uintptr_t, int);
-  probe vthread__thaw(uintptr_t, int);
+  probe vthread__freeze(uintptr_t, int, uintptr_t);
+  probe vthread__thaw(uintptr_t, int, uintptr_t);
 };
 
 #pragma D attributes Evolving/Evolving/Common provider hotspot provider
